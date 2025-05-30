75cfa0633501fb6d67b912286116e77ecdbae7a0
Alexander <alexander.stenberg@reactionalmusic.com>
2025-05-30 15:54:18 +0200

    pytests

--- test/unit/test_detect_duplicates.py
+++ test/unit/test_detect_duplicates.py
@@ -5,3 +5,100 @@ import pytest
 @pytest.mark.unit
 def test_detect_duplicates():
     assert True
+
+@pytest.mark.unit
+def test_detect_duplicates_empty():
+    with pytest.raises(ValueError):
+        from src.util.detector import detect_duplicates
+        detect_duplicates("")
+
+@pytest.mark.unit
+def test_detect_duplicates_single_article():
+    with pytest.raises(ValueError):
+        from src.util.detector import detect_duplicates
+        detect_duplicates("")
+
+@pytest.mark.unit
+def test_detect_duplicates_missing_doi_same_key():
+    from src.util.detector import detect_duplicates
+    data = """
+    @article{key1,
+        title={Article 1},
+        author={Author A},
+        year={2020}
+    }
+    @article{key2,
+        title={Article 2},
+        author={Author B},
+        year={2021}
+    }
+    @article{key1,
+        title={Article 2},
+        author={Author B},
+        year={2021}
+    }
+    """
+    duplicates = detect_duplicates(data)
+    assert len(duplicates) == 2
+
+@pytest.mark.unit
+def test_detect_duplicates_same_key_one_missing_doi():
+    from src.util.detector import detect_duplicates
+    data = """
+    @article{key1,
+        title={Article 1},
+        author={Author A},
+        year={2020}
+    }
+    @article{key1,
+        title={Article 2},
+        author={Author B},
+        year={2021}
+    }
+    @article{key2,
+        title={Article 3},
+        author={Author C},
+        year={2022},
+        doi={10.1000/xyz789}
+    }
+    """
+    duplicates = detect_duplicates(data)
+    assert len(duplicates) == 2
+
+@pytest.mark.unit
+def test_detect_no_duplicates_missing_doi():
+    from src.util.detector import detect_duplicates
+    data = """
+    @article{key1,
+        title={Article 1},
+        author={Author A},
+        year={2020},
+    }
+    @article{key2,
+        title={Article 2},
+        author={Author B},
+        year={2021},
+    }
+    """
+    duplicates = detect_duplicates(data)
+    assert len(duplicates) == 0
+
+@pytest.mark.unit
+def test_detect_duplicates_same_doi():
+    from src.util.detector import detect_duplicates
+    data = """
+    @article{key1,
+        title={Article 1},
+        author={Author A},
+        year={2020},
+        doi={10.1000/xyz123}
+    }
+    @article{key2,
+        title={Article 2},
+        author={Author B},
+        year={2021},
+        doi={10.1000/xyz123}
+    }
+    """
+    duplicates = detect_duplicates(data)
+    assert len(duplicates) == 2
